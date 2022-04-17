3ddddd950e8c9bb327b877474fea396ac2791336
Biahassen <97188207+Biahassen@users.noreply.github.com>
2022-04-17 23:50:42 +0300

    Update personaldata.jsx

--- src/components/PersonalData/personaldata.jsx
+++ src/components/PersonalData/personaldata.jsx
@@ -11,19 +11,19 @@ const Personaldata = () => (
     </div>
     <div className="app__newsletter-input">
     <p className="p__opensans">First name</p>
-      <input type="email" placeholder="Enter your first name" />
+      <input type="email" placeholder="Rabia" />
       <p className="p__opensans">Last name</p>
-      <input type="email" placeholder="Enter your last name" />
+      <input type="email" placeholder="Hassen" />
       <p className="p__opensans">Age</p>
-      <input type="email" placeholder="Enter your age" />
+      <input type="email" placeholder="21" />
       <p className="p__opensans">Sex</p>
-      <input type="email" placeholder="Enter your sex" />
+      <input type="email" placeholder="female" />
       <p className="p__opensans">Address</p>
-      <input type="email" placeholder="Enter your address" />
-      <p className="p__opensans">Marital Status</p>
-      <input type="email" placeholder="Enter your marital status" />
-      <p className="p__opensans">Nationality</p>
-      <input type="email" placeholder="Enter your nationality" />
+      <input type="email" placeholder="Addis Ababa" />
+      <p className="p__opensans">Email</p>
+      <input type="email" placeholder="rabhas4426@gmail.com" />
+      <p className="p__opensans">Phone number</p>
+      <input type="email" placeholder="0944193139" />
     </div>
   </div>
 );
