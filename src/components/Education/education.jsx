20cacd88fe402ee1422a8d91de243e1fca364b62
Biahassen <97188207+Biahassen@users.noreply.github.com>
2022-04-17 23:56:59 +0300

    Update education.jsx

--- src/components/Education/education.jsx
+++ src/components/Education/education.jsx
@@ -11,13 +11,13 @@ const Education = () => (
     <div className="app__newsletter-input">
       
     <p className="p__opensans">Elementary school</p>
-      <input type="email" placeholder="Enter where you attended" />
+      <input type="email" placeholder="Andelus 2002-2006" />
       <p className="p__opensans">Primary school</p>
-      <input type="email" placeholder="Enter where you attended" />
+      <input type="email" placeholder="Hilltops 2007-2012" />
       <p className="p__opensans">Highschool</p>
-      <input type="email" placeholder="Enter where you attended" />
+      <input type="email" placeholder="Spring of knowledge 2013-2018" />
       <p className="p__opensans">Higher Education</p>
-      <input type="email" placeholder="Enter where you attended" />
+      <input type="email" placeholder="AASTU 2018-2022" />
     </div>
   </div>
 );
